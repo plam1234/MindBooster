ccb89b9164f5a33098e1b01bf06f2625714108f6
Peter Lam <Piitaa1234@gmail.com>
2020-06-10 12:35:40 -0400

    changing to standard folders

---
+++ client/build/precache-manifest.5faa56424f7731d002390e273c9cd358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "81fd9257f0e158e2b304",
+    "url": "/static/js/main.e731e796.chunk.js"
+  },
+  {
+    "revision": "c7df09a61a2c37d2f3a9",
+    "url": "/static/js/2.5c25870c.chunk.js"
+  },
+  {
+    "revision": "81fd9257f0e158e2b304",
+    "url": "/static/css/main.5cafa0ea.chunk.css"
+  },
+  {
+    "revision": "d24d1faf7deed433cd3e279b89df6415",
+    "url": "/index.html"
+  }
+];
