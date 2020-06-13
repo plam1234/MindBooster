de1490e6b1cfde70275c44eacdc1852882d08e17
Mikhail Mednikh <mmednikh@gmail.com>
2020-06-12 20:39:50 -0400

    styly with quiz page

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -1,77 +1,85 @@
-import React, {Component} from "react"; 
-import ReactDOM from "react-dom"; 
-import questionAPI from '../question/index'; 
-import QuestionBox from '../components/QuestionBox'; 
-import Result from '../components/ResultBox'; 
-import "../style.css"; 
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import questionAPI from "../question/index";
+import QuestionBox from "../components/QuestionBox";
+import Result from "../components/ResultBox";
+import "../style.css";
+import logo from "../logo.png";
 
-class Quiz extends Component { 
-constructor() { 
-	super(); 
-	this.state = { 
-	questionBank: [], 
-	score: 0, 
-	responses: 0 
-	}; 
-} 
+class Quiz extends Component {
+    constructor() {
+        super();
+        this.state = {
+            questionBank: [],
+            score: 0,
+            responses: 0,
+        };
+    }
 
-// Function to get question from ./question 
-getQuestions = () => { 
-	questionAPI().then(question => { 
-	this.setState({questionBank: question}); 
-	}); 
-}; 
+    // Function to get question from ./question
+    getQuestions = () => {
+        questionAPI().then((question) => {
+            this.setState({ questionBank: question });
+        });
+    };
 
-// Set state back to default and call function 
-playAgain = () => { 
-	this.getQuestions(); 
-	this.setState({score: 0, responses: 0}); 
-}; 
+    // Set state back to default and call function
+    playAgain = () => {
+        this.getQuestions();
+        this.setState({ score: 0, responses: 0 });
+    };
 
-// Function to compute scores 
-computeAnswer = (answer, correctAns) => { 
-	if (answer === correctAns) { 
-	this.setState({ 
-		score: this.state.score + 1 
-	}); 
-	} 
-	this.setState({ 
-	responses: this.state.responses < 5 
-		? this.state.responses + 1 
-		: 5 
-	}); 
-}; 
+    // Function to compute scores
+    computeAnswer = (answer, correctAns) => {
+        if (answer === correctAns) {
+            this.setState({
+                score: this.state.score + 1,
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.responses + 1 : 5,
+        });
+    };
 
-// componentDidMount function to get question 
-componentDidMount() { 
-	this.getQuestions(); 
-} 
+    // componentDidMount function to get question
+    componentDidMount() {
+        this.getQuestions();
+    }
 
-render() { 
-	return (<div className="container"> 
-	<div className="title"> 
-		Mind Booster 
-	</div> 
+    render() {
+        return (
+            <div className="container">
+                <div className="title">
+                    <h1>Quiz of </h1>
+                    <img src={logo} />
+                </div>
 
-	{this.state.questionBank.length > 0 && 
-	this.state.responses < 5 && 
-	this.state.questionBank.map(({question, answers, 
-	correct, questionId}) => <QuestionBox question= 
-	{question} options={answers} key={questionId} 
-	selected={answer => this.computeAnswer(answer, correct)}/>)} 
+                {this.state.questionBank.length > 0 &&
+                    this.state.responses < 5 &&
+                    this.state.questionBank.map(
+                        ({ question, answers, correct, questionId }) => (
+                            <QuestionBox
+                                question={question}
+                                options={answers}
+                                key={questionId}
+                                selected={(answer) =>
+                                    this.computeAnswer(answer, correct)
+                                }
+                            />
+                        )
+                    )}
 
-	{ 
-		this.state.responses === 5 
-		? (<Result score={this.state.score} 
-			playAgain={this.playAgain}/>) 
-		: null
-	} 
+                {this.state.responses === 5 ? (
+                    <Result
+                        score={this.state.score}
+                        playAgain={this.playAgain}
+                    />
+                ) : null}
+            </div>
+        );
+    }
+}
 
-	</div>) 
-} 
-} 
-
-ReactDOM.render(<Quiz/>, document.getElementById("root")); 
-
+ReactDOM.render(<Quiz />, document.getElementById("root"));
 
 export default Quiz;
