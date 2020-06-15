75621845b70e2450fe03921b840b77bf4fe82bbf
Peter Lam <Piitaa1234@gmail.com>
2020-06-15 15:56:26 -0400

    added mlab database information

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-  mongoURI: "mongodb://127.0.0.1:27017/users",
+  // mongoURI: "mongodb://127.0.0.1:27017/users",
+  mongoURI:
+    "mongodb://<user>:<password>@ds021922.mlab.com:21922/heroku_89j2lgj2",
   secretOrKey: "secret",
 };
