9243d1274ca50c53c33280a6b80a5726f2cc3f21
Mikhail Mednikh <mmednikh@gmail.com>
2020-06-16 01:35:17 -0400

    style for answer page

--- client/src/components/ResultBox.js
+++ client/src/components/ResultBox.js
@@ -8,9 +8,8 @@ const Result = ({ score, answer, playAgain }) => (
         </div>
         <div className="ans">
             {" "}
-            The Correct Answers Are 1.{answer[0]}, 2.{answer[1]}, 3.{answer[2]},
-            4.
-            {answer[3]}, 5.{answer[4]}{" "}
+            The Correct Answers Are (1 - {answer[0]}) (2 - {answer[1]}) (3 -
+            {answer[2]}) (4 -{answer[3]}) (5 - {answer[4]}){" "}
         </div>
         <button className="playBtn" onClick={playAgain}>
             {" "}
