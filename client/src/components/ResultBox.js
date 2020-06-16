ffc316ca90e893eca61ebef26fcdc93cc5e0fe66
Mikhail Mednikh <mmednikh@gmail.com>
2020-06-16 00:51:52 -0400

    try to add user

--- client/src/components/ResultBox.js
+++ client/src/components/ResultBox.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "../style.css";
 
-const Result = ({ score, playAgain }) => (
-  <div className="score-board">
-    <div className="score">
-      {" "}
-      Your score is {score} / 5 correct answer ! ! !{" "}
+const Result = ({ user, score, playAgain }) => (
+    <div className="score-board">
+        <div className="user">Hey {user} your grate is</div>
+        <div className="score">
+            {" "}
+            Your score is {score} / 5 correct answer ! ! !{" "}
+        </div>
+
+        <button className="playBtn" onClick={playAgain}>
+            {" "}
+            Play Again{" "}
+        </button>
     </div>
-    <button className="playBtn" onClick={playAgain}>
-      {" "}
-      Play Again{" "}
-    </button>
-  </div>
 );
 
 export default Result;
