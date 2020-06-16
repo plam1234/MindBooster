42e6de59732456b9af951dad7ba90295d31a17bf
Peter Lam <Piitaa1234@gmail.com>
2020-06-15 21:25:22 -0400

    added process.env for mongo

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
-  // mongoURI: "mongodb://127.0.0.1:27017/users",
-  mongoURI:
-    "mongodb://<user>:<password>@ds021922.mlab.com:21922/heroku_89j2lgj2",
+  mongoURI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/users",
+
   secretOrKey: "secret",
 };
