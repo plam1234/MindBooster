34e482c05f49758c9b28668b41d7528a4d03e842
Mikhail Mednikh <mmednikh@gmail.com>
2020-06-16 11:59:02 -0400

    footer and style

--- client/src/components/Nav/Footer.js
+++ client/src/components/Nav/Footer.js
@@ -5,21 +5,7 @@ import "../../App";
 const Footer = () => {
     return (
         <div className="main-footer">
-            <p>
-                {" "}
-                Made with{" "}
-                <i
-                    class="em em-heartpulse"
-                    aria-role="presentation"
-                    aria-label="GROWING HEART"
-                ></i>
-                by / Peter Lam / Vimal Rampaul / Mikhail Mednikh /
-                <i
-                    class="em em-blush"
-                    aria-role="presentation"
-                    aria-label="SMILING FACE WITH SMILING EYES"
-                ></i>
-            </p>
+            <p>Made by / Peter Lam / Vimal Rampaul / Mikhail Mednikh /</p>
         </div>
     );
 };
