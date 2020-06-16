30fd77a43078ee7558d66c0d5cc040e640b2c9a3
Vimal Rampaul <vrampaul@icloud.com>
2020-06-16 00:48:58 -0400

    displays correct answers

--- client/src/components/ResultBox.js
+++ client/src/components/ResultBox.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../style.css";
 
-const Result = ({ score, playAgain }) => (
+const Result = ({ score, answer, playAgain }) => (
   <div className="score-board">
     <div className="score">
       {" "}
       Your score is {score} / 5 correct answer ! ! !{" "}
+    </div>
+    <div className="ans">
+      {" "}
+      The Correct Answers Are 1.{answer[0]}, 2.{answer[1]}, 3.{answer[2]}, 4.{answer[3]}, 5.{answer[4]} {" "}
     </div>
     <button className="playBtn" onClick={playAgain}>
       {" "}
